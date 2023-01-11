82fba63148252530869dbdca71d0e6d3323a835c
NERFOO <james.jaime.1105@gmail.com>
2023-01-11 09:58:11 +0100

    Add project files.

---
+++ 01_Web_Fundamentos/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace _01_Web_Fundamentos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPulsar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPulsar
+            // 
+            this.btnPulsar.Location = new System.Drawing.Point(51, 106);
+            this.btnPulsar.Name = "btnPulsar";
+            this.btnPulsar.Size = new System.Drawing.Size(271, 42);
+            this.btnPulsar.TabIndex = 0;
+            this.btnPulsar.Text = "Pulsar";
+            this.btnPulsar.UseVisualStyleBackColor = true;
+            this.btnPulsar.Click += new System.EventHandler(this.btnPulsar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(193, 56);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(129, 23);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 562);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnPulsar);
+            this.Name = "Form1";
+            this.Text = "NERFO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnPulsar;
+        private TextBox txtNombre;
+        private Label label1;
+    }
+}
---
+++ 01_Web_Fundamentos/Form1.cs
@@ -0,0 +1,26 @@
+namespace _01_Web_Fundamentos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPulsar_Click(object sender, EventArgs e)
+        {
+            //PROPIEDAD DE TIPO PRIMITIVO
+            this.txtNombre.Width = 150;
+            this.btnPulsar.Text = "Boton pulsado!!";
+
+            //PROPIEDAD DE TIPO OBJETO
+            this.txtNombre.Location = new Point(70, 100);
+
+            //PROPIEDAD DE TIPO ENUMERADAS
+            //UNA ENUMERACION SON UNA SERIE DE VALORES QUE SE REPRESENTAN DE FORMA GRAFICA Y AYUDAN AL PROGRAMADOR
+            this.txtNombre.TextAlign = HorizontalAlignment.Center;
+            this.BackColor = Color.AliceBlue;
+        }
+
+    }
+}
